dd25d4260c31da34a312ba5bdc126c985a3de9e6
João Melo <jgvm@academy.cin.ufpe.br>
2019-04-23 10:05:29 -0300

    First Cloud implementation

--- bosque/bosque/ViewController.swift
+++ bosque/bosque/ViewController.swift
@@ -4,35 +4,29 @@
 //
 //  Created by João Melo on 15/04/19.
 //  Copyright © 2019 João Melo. All rights reserved.
-//
-//O QUE TEM QUE FAZER:
-// 1. ENVIAR QUE UM CLIQUE NO BOTÃO FOI DADO
-// 2. ATUALIZAR O DADO NA CLOUD
-// 3. FAZER O FETCH DO NOVO NÚMERO TOTAL
-// 4. GERAR OBJETOS DE ACORDO COM O NÚMERO TOTAL EM LOCAIS DIFERENTES DA TELA
-// ??
-// PROFIT
 
 
 import UIKit
 import CloudKit
 
 class ViewController: UIViewController {
-
+    
     var container = CKContainer.default()
     var publicDB = CKContainer.default().publicCloudDatabase
     var treeInsert = "new tree"
     var trees = [CKRecord]()
-    let treeImage = CAShapeLayer()
     var xAxis = 187.5
     var yAxis = 333.5
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.loadTrees()
+        self.loadTrees(createTree: false)
+        for tree in trees {
+            self.createTree()
+        }
     }
-
+    
     @IBAction func updateTreeCount(_ sender: Any) {
         self.saveTree(string: self.treeInsert)
     }
@@ -46,11 +40,11 @@ class ViewController: UIViewController {
             guard record != nil else { return }
             print("new tree saved")
             print("trees.count na saveTree \(self.trees.count)")
-            self.loadTrees()
+            self.loadTrees(createTree: true)
         }
     }
     
-    func loadTrees() {
+    public func loadTrees(createTree: Bool) {
         print("entrou no load")
         let query = CKQuery(recordType: "Tree", predicate: NSPredicate(value: true))
         self.publicDB.perform(query, inZoneWith: nil) { (records, _) in
@@ -59,7 +53,7 @@ class ViewController: UIViewController {
             DispatchQueue.main.async {
                 print(self.yAxis)
                 print("trees.count na loadTrees \(self.trees.count)")
-                for tree in self.trees {
+                if createTree {
                     self.createTree()
                     self.yAxis += 10
                 }
@@ -69,12 +63,13 @@ class ViewController: UIViewController {
     
     func createTree() {
         let circularPath = UIBezierPath(arcCenter: .zero, radius: 40.0, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
-        self.treeImage.path = circularPath.cgPath
-        self.treeImage.lineWidth = 2.0
-        self.treeImage.fillColor = UIColor.clear.cgColor
-        self.treeImage.strokeColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
-        self.treeImage.lineCap = CAShapeLayerLineCap.round
-        self.treeImage.position = CGPoint(x: self.xAxis, y: self.yAxis)
+        let treeImage = CAShapeLayer()
+        treeImage.path = circularPath.cgPath
+        treeImage.lineWidth = 2.0
+        treeImage.fillColor = UIColor.clear.cgColor
+        treeImage.strokeColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        treeImage.lineCap = CAShapeLayerLineCap.round
+        treeImage.position = CGPoint(x: self.xAxis, y: self.yAxis)
         self.view.layer.addSublayer(treeImage)
     }
 }
